53948b433414126c34de7426b1d8fe95dfe4b34f
Pank3d <maxpannk3d@mail.ru>
2024-03-04 22:22:45 +0300

    first commit

--- vitacore/src/App.tsx
+++ vitacore/src/App.tsx
@@ -1,15 +1,24 @@
 import { useEffect, useState } from "react";
 import { dataInter } from "./shared/type";
 import { getInfo } from "./shared/api";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from "recharts";
 
 function App() {
-  const [gariDate, setGariDate] = useState<dataInter[]>([]); 
+  const [gariDate, setGariDate] = useState<dataInter[]>([]);
   const [grif, setGrif] = useState<string[]>([]);
   const [sliz, setSliz] = useState<string[]>([]);
   const [puff, setPuff] = useState<string[]>([]);
   const [raven, setRaven] = useState<string[]>([]);
-  const [student, setStudent] = useState<string[]>([]);
-  
+  const [startDate, setStartDate] = useState<string>('');
+  const [endDate, setEndDate] = useState<string>('');
 
   useEffect(() => {
     const fetchData = async () => {
@@ -24,69 +33,100 @@ function App() {
   }, []);
 
   useEffect(() => {
-    const grifMembers = gariDate?.filter(
+    if (gariDate.length === 0) return;
+
+    const grifMembers = gariDate.filter(
       (member: dataInter) =>
         member.house === "Gryffindor" &&
-        member.hogwartsStudent === true  &&
-        member.dateOfBirth !== null 
+        member.hogwartsStudent === true/*  &&
+        member.dateOfBirth !== null */
     );
-    const grifDatesOfBirth = grifMembers?.map(
-      (member: dataInter) => member.dateOfBirth
+    const grifDatesOfBirth = grifMembers.map((member: dataInter) =>
+       (member.dateOfBirth)  
     );
-    const slizMembers = gariDate?.filter(
+
+    const slizMembers = gariDate.filter(
       (member: dataInter) =>
         member.house === "Slytherin" &&
-        member.hogwartsStudent === true &&
-        member.dateOfBirth !== null
+        member.hogwartsStudent === true /* &&
+        member.dateOfBirth !== null */
     );
-    const slizDatesOfBirth = slizMembers?.map(
-      (member: dataInter) => member.dateOfBirth
+    const slizDatesOfBirth = slizMembers.map((member: dataInter) =>
+       (member.dateOfBirth)  
     );
 
-    const ravenMembers = gariDate?.filter(
+    const ravenMembers = gariDate.filter(
       (member: dataInter) =>
-        member.house === "Gryffindor" &&
-        member.hogwartsStudent === true &&
-        member.dateOfBirth !== null
+        member.house === "Ravenclaw" &&
+        member.hogwartsStudent === true /* &&
+        member.dateOfBirth !== null */
     );
-    const ravenDatesOfBirth = ravenMembers?.map(
-      (member: dataInter) => member.dateOfBirth
+    const ravenDatesOfBirth = ravenMembers.map((member: dataInter) =>
+       (member.dateOfBirth)  
     );
 
-     const puffMembers = gariDate?.filter(
-       (member: dataInter) =>
-         member.house === "Gryffindor" &&
-         member.hogwartsStudent === true &&
-         member.dateOfBirth !== null
-     );
-     const puffDatesOfBirth = puffMembers?.map(
-       (member: dataInter) => member.dateOfBirth
-     );
-    
-    
+    const puffMembers = gariDate.filter(
+      (member: dataInter) =>
+        member.house === "Hufflepuff" &&
+        member.hogwartsStudent === true /* &&
+        member.dateOfBirth !== null */
+    );
+    const puffDatesOfBirth = puffMembers.map((member: dataInter) =>
+       (member.dateOfBirth)  
+    );
 
-    const studentsMemb = gariDate.filter(
-      (member:dataInter) => member.hogwartsStudent 
-    )
-
-    const studentsTrue = studentsMemb.map((member:dataInter) => member.name)
-
-
-
-    setStudent(studentsTrue || [])
     setGrif(grifDatesOfBirth || []);
-    setSliz(slizDatesOfBirth || [])
-    setRaven(ravenDatesOfBirth || [])
-    setPuff(puffDatesOfBirth || [])
+    setSliz(slizDatesOfBirth || []);
+    setRaven(ravenDatesOfBirth || []);
+    setPuff(puffDatesOfBirth || []);
   }, [gariDate]);
 
-  console.log("факультет гриф", grif)
-  console.log("факультет слизи", sliz)
-  console.log("факультет raven", raven)
-  console.log("факультет пуфф", puff)
-  console.log(student)
+  console.log("факультет гриф", grif);
+  console.log("факультет слизи", sliz);
+  console.log("факультет raven", raven);
+  console.log("факультет пуфф", puff);
+  console.log(startDate);
+  console.log(endDate);
 
-  return <></>;
+  const allData = [
+    { faculty: "Gryffindor", count: grif.length },
+    { faculty: "Slytherin", count: sliz.length },
+    { faculty: "Ravenclaw", count: raven.length },
+    { faculty: "Hufflepuff", count: puff.length },
+  ];
+
+  return (
+    <>
+      <div>
+        <p>от</p>
+        <input
+          type="date"
+          onChange={(e) => setStartDate(e.target.value)}
+          value={startDate || ""}
+        />
+        <p>до</p>
+        <input
+          type="date"
+          value={endDate || ""}
+          onChange={(e) => setEndDate(e.target.value)}
+        />
+      </div>
+      {endDate === "" && startDate === "" ? (
+        <>
+          <BarChart width={600} height={400} data={allData}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="faculty" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="count" fill="#8884d8" />
+          </BarChart>
+        </>
+      ) : (
+        <>Тут будут отсортированные данные</>
+      )}
+    </>
+  );
 }
 
 export default App;
